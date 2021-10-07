317339889eeb36540fad9d32a846f53408639d2a
jernber <oraclemars@gmail.com>
2021-10-07 16:01:40 -0500

    started work on setting up redux toolkit for apis

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,9 @@
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': '561fe73ee8mshbcae87a2953e69dp10c1b1jsnfe2e4f55f178'
+//     }
+//   };
