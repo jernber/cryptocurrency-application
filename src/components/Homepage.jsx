032e2f1e8e3d85804601cb895deccdd92900868f
jernber <oraclemars@gmail.com>
2021-10-07 17:57:14 -0500

    added more data to home page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -10,8 +10,8 @@ const { Title } = Typography
 
 const Homepage = () => {
     // Redux Toolkit provides isFetching state on API calls
-    const { data, isFetching } = useGetCryptosQuery();
-    const globalStats = data?.data?.stats;
+    const { data, isFetching } = useGetCryptosQuery()
+    const globalStats = data?.data?.stats
 
     console.log(data)
 
@@ -22,10 +22,10 @@ const Homepage = () => {
             <Title level={2} className="heading">Global Crypto Stats</Title>
             <Row>
                 <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
-                <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
-                <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
-                <Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
-                <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
+                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
             </Row>
         </>
     )
