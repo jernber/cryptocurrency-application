4dadda525ef3f829ed8e7d2ed747922decdac09b
jernber <oraclemars@gmail.com>
2021-10-07 15:31:23 -0500

    ending div error

--- src/App.js
+++ src/App.js
@@ -40,17 +40,18 @@ const app = () => {
                             </Switch>
                         </div>
                     </Layout>
-                </div>
+                
                 <div className="footer">
                     <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
                         Cryptoverse <br />
-                        All right reserved
+                        All Rights Reserved
                     </Typography.Title>
                     <Space>
                         <Link to="/">Home</Link>
                         <Link to="/exchanges">Exchanges</Link>
                         <Link to="/news">News</Link>
                     </Space>
+                </div>
                 </div>
             </div>
         </div>
