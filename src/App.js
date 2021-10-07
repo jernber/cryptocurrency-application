ac812824e0f063f3e090f953d1234ca980fff71a
jernber <oraclemars@gmail.com>
2021-10-07 15:15:00 -0500

    Setup latest components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
 
 // Cool trick to save lines by importing components from an index of components
-import { Navbar } from './components'
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components'
 import './App.css'
 
 const app = () => {
@@ -14,7 +14,28 @@ const app = () => {
                     <Navbar />
                 </div>
                 <div className="main">
-
+                    <Layout>
+                        <div className="routes">
+                            {/* Allows multiple routes */}
+                            <Switch>
+                                <Route exact path="/">
+                                    <Homepage />
+                                </Route>
+                                <Route exact path="/exchanges">
+                                    <Exchanges />
+                                </Route>
+                                <Route exact path="/cryptocurrencies">
+                                    <Cryptocurrencies />
+                                </Route>
+                                <Route exact path="/crypto/:coinId">
+                                    <CryptoDetails />
+                                </Route>
+                                <Route exact path="/news">
+                                    <News />
+                                </Route>
+                            </Switch>
+                        </div>
+                    </Layout>
                 </div>
                 <div className="footer">
 
