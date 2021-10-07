6a9a31fcdb2bb7556e47dcb41d0ed0640696672e
jernber <oraclemars@gmail.com>
2021-10-07 15:29:52 -0500

    added to footer

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const app = () => {
                                 <Route exact path="/crypto/:coinId">
                                     <CryptoDetails />
                                 </Route>
-                                
+
                                 <Route exact path="/news">
                                     <News />
                                 </Route>
@@ -42,7 +42,15 @@ const app = () => {
                     </Layout>
                 </div>
                 <div className="footer">
-
+                    <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                        Cryptoverse <br />
+                        All right reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
                 </div>
             </div>
         </div>
