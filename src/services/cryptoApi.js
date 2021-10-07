f9a9a0eec824cf75f45108ca02b8a1499a283f32
jernber <oraclemars@gmail.com>
2021-10-07 16:28:34 -0500

    setup base in redux for calling crypto api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,9 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
 
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': '561fe73ee8mshbcae87a2953e69dp10c1b1jsnfe2e4f55f178'
-//     }
-//   };
+const cryptoApiHeaders = {
+          'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+          'x-rapidapi-key': '561fe73ee8mshbcae87a2953e69dp10c1b1jsnfe2e4f55f178'
+}
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+const baseUrl = 'https://coinranking1.p.rapidapi.com/exchanges'
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/exchanges')
+        })
+    })
+})  
