224783ee78c8038728dc9bc777d70bad4f4d143b
jernber <oraclemars@gmail.com>
2021-10-08 17:11:35 -0500

    updated api for time period

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Line } from 'react-chartjs-2'
 import { Col, Row, Typography } from 'antd'
 
+const { Title } = Typography;
+
 const LineChart = () => {
     return (
         <div>
