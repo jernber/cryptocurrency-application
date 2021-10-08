1f5e6b582aee1bdfb1f17c131af7b75fafbaec4c
jernber <oraclemars@gmail.com>
2021-10-08 15:20:12 -0500

    created cryptoNews API

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { cryptoApi } from "../services/cryptoApi";
+import { cryptoNewsApi } from "../services/cryptoNewsApi";
 
 // export default configureStore({
 //     reducer: {
@@ -10,5 +11,6 @@ import { cryptoApi } from "../services/cryptoApi";
 export default configureStore({
     reducer: {
         [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
     },
 })
