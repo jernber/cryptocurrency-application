73f5eeebe19cdb8bbdf0b84bc59d97008be1c23d
jernber <oraclemars@gmail.com>
2021-10-08 15:35:55 -0500

    fixed issue where images are overflowing

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -22,7 +22,7 @@ const News = ({ simplified }) => {
                         <a href={news.url} target="_blank" rel="noreffer">
                             <div className="news-image-container">
                                 <Title className="news-title" level={4}>{news.name}</Title>
-                                <img  src={news?.image?.thumbnail.contentUrl || demoImage } alt="news" />
+                                <img style={{maxWidth: '200px', maxHeight: '100px'}} src={news?.image?.thumbnail.contentUrl || demoImage } alt="news" />
                             </div>
                             <p>
                                 {news.description > 100 ? `${news.description.substring(0,100)}` : news.description}
