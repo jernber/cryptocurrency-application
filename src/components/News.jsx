972a1f8a82d8754c96b5fb2bc0fa133d70874c5e
jernber <oraclemars@gmail.com>
2021-10-08 15:45:26 -0500

    search based off selected crypto

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,6 +3,7 @@ import { Select, Typography, Row, Col, Avatar, Card } from 'antd'
 import moment from 'moment'
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const { Text, Title } = Typography
 const { Option } = Select
@@ -10,9 +11,10 @@ const { Option } = Select
 const demoImage = 'https://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg'
 
 const News = ({ simplified }) => {
-    const [newsCategory, setnewsCategory] = useState('Cryptocurrency')
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
     const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: newsCategory, count: simplified ? 6 : 12 })
-    
+    const { data } = useGetCryptosQuery(100)
+
     if(!cryptoNews?.value) return 'Loading...'
 
     return (
@@ -24,10 +26,11 @@ const News = ({ simplified }) => {
                         className="select-news"
                         placeholder="Select a Crypto"
                         optionFilterProp="children"
-                        onChange={(value) => console.log(value)}
+                        onChange={(value) => setNewsCategory(value)}
                         filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}    
                     >
-
+                        <Option value="Cryptocurrency">Cryptocurrency</Option>
+                        {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option>)}
                     </Select>
                 </Col>
             )}
