526ab1112d1868950d7b6db2bc770e9734ac2850
jernber <oraclemars@gmail.com>
2021-10-07 22:29:31 -0500

    added design to cryptocurrencies page

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import { cryptoApi } from "../services/cryptoApi";
 
+// export default configureStore({
+//     reducer: {
+//         [cryptoApi.reducerPath]: cryptoApi.reducer
+//     },
+// })
 export default configureStore({
     reducer: {
-        [cryptoApi.reducerPath]: cryptoApi.reducer
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
     },
 })
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
 
 import { useGetCryptosQuery } from '../services/cryptoApi'
+import { Cryptocurrencies, News } from '../components'
 
 // Calling Typography.Title alot, just to shorthand it we change it to Title
 const { Title } = Typography
@@ -12,8 +13,6 @@ const Homepage = () => {
     // Redux Toolkit provides isFetching state on API calls
     const { data, isFetching } = useGetCryptosQuery()
     const globalStats = data?.data?.stats
-
-    console.log(data)
 
     if(isFetching) return 'Loading...'
 
@@ -27,6 +26,18 @@ const Homepage = () => {
                 <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
                 <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
             </Row>
+
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+                <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+            </div>
+            <Cryptocurrencies simplified />
+
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={3} className="show-more"><Link to="/news">Show more</Link></Title>
+            </div>
+            <News simplified />
         </>
     )
 }
