de435999d7bba254a2fa59f1e44c4450c972d522
jernber <oraclemars@gmail.com>
2021-10-08 17:36:40 -0500

    finished linechart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
@@ -6,12 +6,31 @@ import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOut
 import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
+    const [activeMenu, setactiveMenu] = useState(true)
+    const [secreenSize, setSecreenSize] = useState(null)
+
+    useEffect(()=> {
+        const handleResize = () => setSecreenSize(window.innerWidth)
+
+        window.addEventListener('resize', handleResize)
+        handleResize()
+        return () => window.removeEventListener('resize', handleResize) 
+    }, [])
+
+    useEffect(() => {
+        if(screenSize < 768){
+            setactiveMenu = false
+        }else{
+            setactiveMenu = true
+        }
+    }, [secreenSize])
+
     return (
         <div className="nav-container">
             <div className="logo-container">
                 <Avatar src={icon} size="large" />
                 <Typography.Title level={2} className="logo">
-                    <Link to="/">Cryptoverse</Link>
+                    <Link to="/">CryptoCave</Link>
                 </Typography.Title>
                 </div>
                 <Menu theme="dark">
